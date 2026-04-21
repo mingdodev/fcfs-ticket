d311496c9d96781262fa01338390de3088715a18
mingdodev <mingdo.dev@gmail.com>
2026-04-22 01:15:48 +0900

    프로젝트 세팅 및 문서화

---
+++ src/main/java/com/example/fcfsticket/FcfsTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.fcfsticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcfsTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcfsTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fcfsticket/FcfsTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcfsticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcfsTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
